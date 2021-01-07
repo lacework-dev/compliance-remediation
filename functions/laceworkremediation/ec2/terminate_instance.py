e6b8036b6a245d3300df44dfd393c8bd6d23eb94
Alan Nix <alan.nix@lacework.net>
2021-01-07 13:18:10 -0500

    Added initial EC2 actions and remediations for no Tags
    - Added EC2 remediations actions for stop and terminate.
    - Added a response action for 'LW_AWS_GENERAL_SECURITY_1' to stop un-tagged instances.
    - Added appropriate AWS IAM permissions to allow the Lambda to interact with EC2.
    - Added tests for EC2 actions.

---
+++ functions/laceworkremediation/ec2/terminate_instance.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Lacework Remediation: terminate_instance.py
+
+This function will terminate an EC2 instance.
+"""
+
+import logging
+
+import boto3
+
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+
+
+def run_action(entity, response):
+    logger.info("Initiating termination of EC2 instance.")
+
+    # Get the region for the resource
+    region = entity.split(":")[3]
+
+    # Create an EC2 client
+    ec2 = boto3.client("ec2", region_name=region)
+
+    try:
+        # Get the instance ID (https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html)
+        instance = entity.split(":")[5].replace("instance/", "")
+
+        # Stop the instance
+        result = ec2.terminate_instances(InstanceIds=[instance])
+
+        response_code = result["ResponseMetadata"]["HTTPStatusCode"]
+
+        if response_code >= 400:
+            # Log the returned error
+            message = f"Unexpected error: {result}"
+            logger.info(message)
+            response["messages"].append(message)
+        else:
+            # Log the successful termination
+            message = f"Instance '{instance}' was successfully terminated."
+            logger.info(message)
+            response["status"] = "ok"
+            response["messages"].append(message)
+
+    except ClientError as e:
+        message = f"Unexpected error: {e}"
+        logger.error(message)
+        response["messages"].append(message)
---
+++ functions/tests/ec2/test_ec2.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+EC2 tests for Lacework Event Router.
+"""
+
+import boto3
+
+from laceworkremediation.lacework_event_router import event_handler
+
+from moto import mock_ec2
+from tests.test_event_data import (
+    build_aws_gen_sec_1_event
+)
+
+REGION = "us-west-1"
+
+
+@mock_ec2
+def create_instance(region):
+
+    ec2 = boto3.resource("ec2", region_name=region)
+
+    # Create a new EC2 instance
+    instances = ec2.create_instances(
+        ImageId="ami-00b6a8a2bd28daf19",
+        MinCount=1,
+        MaxCount=2
+    )
+
+    instance_id = instances[0].id
+
+    return instance_id
+
+
+@mock_ec2
+def test_lambda_handler_aws_gen_sec_1():
+
+    # Create an EC2 Instance
+    instance_id = create_instance(REGION)
+
+    # Synthesize an event for the instance
+    event_string = build_aws_gen_sec_1_event(instance_id, REGION)
+
+    try:
+        response = event_handler(event=event_string, context={})
+    except Exception as e:
+        print(e)
+
+    assert response["status"] == "ok"
